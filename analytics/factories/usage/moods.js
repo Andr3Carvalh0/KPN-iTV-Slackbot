1232600e4c33672e40b8a091be5006aac73eadc7
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-03-08 18:43:44 +0100

    * Randomize emojis on the moods view

--- analytics/factories/usage/moods.js
+++ analytics/factories/usage/moods.js
@@ -1,11 +1,24 @@
+const collections = require('./../../../utilities/collections/collections.js')
 const common = require('./../common.js')
+
+const emojis = [
+    ":kpn::snowflake:",
+    ":wine_glass::kissing_heart:",
+    ":saxophone::sunrise:",
+    ":no_good::jeans:",
+    ":microphone::musical_note:"
+]
+
+function mood() {
+    return collections.shuffle(emojis)[0]
+}
 
 module.exports = {
     id: function () {
         return "moods"
     },
     title: function () {
-        return ":kpn::snowflake: *Popular Moods:*"
+        return `${mood()} *Popular Moods:*`
     },
     message: function (data) {
         return common.output(data.moods, {
